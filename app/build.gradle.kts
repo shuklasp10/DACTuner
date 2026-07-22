53d4c06eaa0940444bd87a20d5d466ba71b140c3
Shri Prakash Shukla <shriprakashshukla8@gmail.com>
2026-07-23 03:21:09 +0530

    chore: configure release plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0-alpha01"
+        versionName = project.version.toString()
     }
 
     buildTypes {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("net.researchgate.release") version "3.0.2"
 }
