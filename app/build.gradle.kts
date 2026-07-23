84cd45cd386f9d0dc2ac80c2934a0baba5a5da7a
Shri Prakash Shukla <shriprakashshukla8@gmail.com>
2026-07-23 18:48:19 +0530

    chore: release 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dactuner"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
     }
 
     buildTypes {
